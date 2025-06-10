42bea929b0f62d6adc9c42d4ca368b7f0043e45d
mimi <110226866+hongkikii@users.noreply.github.com>
2025-06-10 13:36:33 +0900

    chore: 초기 데이터 설정

--- src/main/java/finalmission/time/domain/Time.java
+++ src/main/java/finalmission/time/domain/Time.java
@@ -3,6 +3,7 @@ package finalmission.time.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalTime;
@@ -11,7 +12,7 @@ import java.time.LocalTime;
 @Table(name = "times")
 public class Time {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,18 @@
+-- USER
+INSERT INTO users (name) VALUES ('mimi');
+
+-- TIME
+INSERT INTO times (time) VALUES ('10:00');
+INSERT INTO times (time) VALUES ('10:30');
+INSERT INTO times (time) VALUES ('11:00');
+INSERT INTO times (time) VALUES ('11:30');
+INSERT INTO times (time) VALUES ('12:00');
+INSERT INTO times (time) VALUES ('12:30');
+INSERT INTO times (time) VALUES ('13:00');
+INSERT INTO times (time) VALUES ('13:30');
+
+-- RESTAURANT
+INSERT INTO restaurants (name) VALUES ('도넛맛집');
+INSERT INTO restaurants (name) VALUES ('국밥맛집');
+INSERT INTO restaurants (name) VALUES ('삼겹살맛집');
+INSERT INTO restaurants (name) VALUES ('곱창맛집');
